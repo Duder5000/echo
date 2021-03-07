697a2fa0cdcc8556e96c4999fac1fa8ba06b6059
Alex <soames.alex@gmail.com>
2021-03-07 15:59:29 -0800

    Starting

---
+++ a3.cpp
@@ -0,0 +1,65 @@
+// a3.cpp
+
+/////////////////////////////////////////////////////////////////////////
+//
+// Student Info
+// ------------
+//
+// Name : Alex Soames
+// St.# : 301 292 784
+// Email: asoames@sfu.ca
+//
+//
+// Statement of Originality
+// ------------------------
+//
+// All the code and comments below are my own original work. For any non-
+// original work, I have provided citations in the comments with enough
+// detail so that someone can see the exact source and extent of the
+// borrowed work.
+//
+// In addition, I have not shared this work with anyone else, and I have
+// not seen solutions from other students, tutors, websites, books,
+// etc.
+//
+/////////////////////////////////////////////////////////////////////////
+
+#include "cmpt_error.h"    // These are the only permitted includes!
+#include "Chatbot.h"       //
+#include <iostream>        // Don't include anything else.
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <chrono>
+#include <ctime>     
+#include <cassert>
+
+using namespace std;
+
+//
+// ... write your code here here ...
+//
+
+// converse is a helper function that you can use if you like. You are free to
+// modify it in any way.
+void converse(Chatbot* a, Chatbot* b, int max_turns = 50) {
+  for(int turn = 1; turn <= max_turns; turn++) {
+    string a_msg = a->get_reply();
+    cout << "(" << turn << ") " << a->name() << ": " << a_msg << "\n";
+    
+    turn++;
+    if (turn > max_turns) return;
+
+    b->tell(a_msg);
+    string b_msg = b->get_reply();
+
+    cout << "(" << turn << ") " << b->name() << ": " << b_msg << "\n";
+    a->tell(b_msg);
+  } // for
+}
+
+int main() {
+  cout << "CMPT 135 - A03\n";
+}
